e69cb3b1cb7ad5110d64e7dc760c640a318b8ee8
mikebender <mikebender@deephaven.io>
2022-01-20 11:00:06 -0500

    Add a deephaven.mplstyle file
    Use our own styling so it looks more like Deephaven

--- src/deephaven/plugin/matplotlib/figure_type.py
+++ src/deephaven/plugin/matplotlib/figure_type.py
@@ -1,8 +1,15 @@
 from io import BytesIO
 from matplotlib.figure import Figure
 from deephaven.plugin.object import Exporter, ObjectType
+import os
+import matplotlib.pyplot as plt
 
 NAME = "matplotlib.figure.Figure"
+
+plt.style.use([
+    'dark_background',
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), 'styles/deephaven.mplstyle') ]
+)
 
 class FigureType(ObjectType):
     @property
