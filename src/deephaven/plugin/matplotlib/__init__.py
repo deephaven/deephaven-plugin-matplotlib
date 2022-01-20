b78dfa8c9384045989162d9343beba863116ce74
Mike Bender <mikebender@deephaven.io>
2022-01-20 16:07:41 -0500

    Move theme initialization to it's own method, add a comment

--- src/deephaven/plugin/matplotlib/__init__.py
+++ src/deephaven/plugin/matplotlib/__init__.py
@@ -4,11 +4,18 @@ import matplotlib.pyplot as plt
 
 __version__ = "0.0.1.dev5"
 
+def init_theme():
+    # Set the Deephaven style globally.
+    # We use the savefig function to export the Figure, and that uses the Figure's properties for colours rather than temporary styling.
+    # The Figure's properties are set on creation time of the Figure, rather than when the Figure is exported
+    # We do not have hooks into when a user creates a new Figure, so we set the theme globally ahead of time
+    # https://github.com/matplotlib/matplotlib/issues/6592/
+    with resources.path(__package__, 'deephaven.mplstyle') as p:
+        plt.style.use(['dark_background',p])
+
 class MatplotlibRegistration(Registration):
     @classmethod
     def register_into(cls, callback: Registration.Callback) -> None:
-        # Need to set the Deephaven style here. When using savefig to export the figure, it reads from the global state instead of temporary style sheets
-        with resources.path(__package__, 'deephaven.mplstyle') as p:
-            plt.style.use(['dark_background',p])
+        init_theme()
         from . import figure_type
         callback.register(figure_type.FigureType)
