c42e30fa4ebb004f9310234a53dd581a2bac4485
Mike Bender <mikebender@deephaven.io>
2022-11-21 18:48:32 -0500

    Use `.to_numpy` instead of `.getDirect()`
    - Python doesn't know how to pickle a java array
    - Fixes #891
    - Still not optimal, as this will have performance issues on large tables

--- src/deephaven/plugin/matplotlib/__init__.py
+++ src/deephaven/plugin/matplotlib/__init__.py
@@ -1,3 +1,4 @@
+from deephaven import numpy as dhnp
 from deephaven.plugin import Registration
 from deephaven.table_listener import listen
 from importlib import resources
@@ -156,5 +157,5 @@ class TableAnimation(Animation):
     def _draw_frame(self, framedata):
         data = {}
         for column in self._columns:
-            data[column] = self._table.j_table.getColumn(column).getDirect()
+            data[column] = dhnp.to_numpy(self._table, [column])
         self._func(data, self._last_update, *self._args)
