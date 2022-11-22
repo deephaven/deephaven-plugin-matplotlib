cddc24c93b827ccd1fb022156bb13b124e564a1c
Mike Bender <mikebender@deephaven.io>
2022-11-22 08:30:31 -0500

    Version bump to 0.1.1

--- src/deephaven/plugin/matplotlib/__init__.py
+++ src/deephaven/plugin/matplotlib/__init__.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import Animation
 import itertools
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 def _init_theme():
     # Set the Deephaven style globally.
